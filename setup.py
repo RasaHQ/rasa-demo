961b2898e094b8e106b4037f71bca0fb30c0ade9
akelad <akela@rasa.ai>
2018-05-30 16:26:14 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='demo',
+        packages=find_packages(),
+        version='0.0.1',
+        install_requires=[
+        ],
+        description="Killer demo",
+        author='Akela Drissner-Schmid',
+        author_email='akela@rasa.com',
+        url='https://github.com/rasahq/killer-demo'
+)
