cc62d2db81afb94c8d9ca94ebf3b611f82600401
Greg Stephens <greg@udon.org>
2019-12-07 08:12:57 -0800

    Initial commit

---
+++ demo/discourse.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DiscourseAPI(object):
+    """Class to connect to the Algolia API"""
+
+    def __init__(self, url):
+        self.url = url
+
+    def query(self, search_string, include_blurbs=False):
+        import requests as r
+
+        params = {'term': search_string, 'include_blurbs': include_blurbs}
+        print(f"query params: {params}, url: {self.url}")
+        res = r.get(url=self.url+"/query.json", params=params)
+        return res
+
+    def search(self, search_string, include_blurbs=False):
+        import requests as r
+
+        params = {'q': search_string}
+        headers={"Content-Type":"application/json; charset=utf-8"}
+        print(f"search params: {params}, url: {self.url}")
+        res = r.get(url=self.url, params=params, headers=headers)
+        return res
