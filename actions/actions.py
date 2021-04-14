9ba03d9b473cb84dd0a8061a61294a08e5a7576a
melindaloubser1 <melinda.loubser@gmail.com>
2021-04-14 11:15:56 +0200

    add trigger response selector

--- actions/actions.py
+++ actions/actions.py
@@ -861,3 +861,22 @@ class ActionTagDocsSearch(Action):
         tag_convo(tracker, label)
 
         return []
+
+
+class ActionTriggerResponseSelector(Action):
+    """Returns the chitchat utterance dependent on the intent"""
+
+    def name(self) -> Text:
+        return "action_trigger_response_selector"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        retrieval_intent = tracker.get_slot("retrieval_intent")
+        if retrieval_intent:
+            dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+
+        return [SlotSet("retrieval_intent", None)]
